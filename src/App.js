143fea5cc468374b710567cbf9cbb7df45a3ca46
Josip Pezer <jpezer@ad.ito.ba>
2022-09-02 11:24:48 +0200

    Add Task component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { useState } from 'react'
 
 import Header from "./components/Header"
 import Tasks from "./components/Tasks"
+import AddTask from './components/AddTask'
 
 
 
@@ -29,14 +30,28 @@ function App() {
 
     //delete task
     const deleteTaks = (id) => {
-        setTasks(tasks.filter((task)=>task.id !== id))
+        setTasks(tasks.filter((task) => task.id !== id))
     }
 
-    //toggle
+    //toggle reminder
+    const toggleReminder = (id) => {
+        setTasks(tasks.map((task) => task.id === id ? { ...task, reminder: !task.reminder } : task))
+    }
+
+    //Add Task
+    const addTask = (task) => {
+        const id = Math.floor(Math.random()*10000)+1;
+        const newTask = {id,...task}
+        setTasks([...tasks,newTask])
+    }
+
     return (
         <div className="container">
             <Header />
-            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTaks} /> : 'No Tasks'}
+            <AddTask onAdd={addTask}/>
+            {tasks.length > 0 ? <Tasks tasks={tasks}
+                onDelete={deleteTaks} onToggle={toggleReminder} />
+                : 'No Tasks'}
         </div>
     );
 }
