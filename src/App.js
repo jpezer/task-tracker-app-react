bfcbbb5c0e676c0d1ed310f51ec88d2e38e120a1
Josip Pezer <jpezer@ad.ito.ba>
2022-09-02 10:39:51 +0200

    first part

---
+++ src/App.js
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+
+import Header from "./components/Header"
+import Tasks from "./components/Tasks"
+
+
+
+function App() {
+    const [tasks, setTasks] = useState([
+        {
+            "id": 1,
+            "text": "Doctors Appointment",
+            "day": "Feb 5th at 2:30pm",
+            "reminder": true
+        },
+        {
+            "id": 2,
+            "text": "Meeting at School",
+            "day": "Feb 6th at 1:30pm",
+            "reminder": true
+        },
+        {
+            "id": 3,
+            "text": "Food Shopping",
+            "day": "Feb 5th at 2:30pm",
+            "reminder": false
+        }
+    ])
+
+    //delete task
+    const deleteTaks = (id) => {
+        setTasks(tasks.filter((task)=>task.id !== id))
+    }
+
+    //toggle
+    return (
+        <div className="container">
+            <Header />
+            {tasks.length > 0 ? <Tasks tasks={tasks} onDelete={deleteTaks} /> : 'No Tasks'}
+        </div>
+    );
+}
+
+export default App;
---
+++ src/components/Tasks.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useState } from 'react'
+
+
+import Task from './Task'
+
+
+const Tasks = ({ tasks,onDelete }) => {
+    return (
+        <div>
+            {tasks.map((task) => (
+                <Task key={task.id} task={task} onDelete={onDelete}/>
+            ))}
+        </div>
+    )
+}
+export default Tasks
